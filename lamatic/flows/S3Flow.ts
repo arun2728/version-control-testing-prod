45f8477dff6619054583d8c5f0a99c49f38c2bf3
accounts-lamatic <accounts@lamatic.ai>
2026-03-15 06:59:19 +0530

    [LAMATIC-COMMIT] Created flow

---
+++ lamatic/flows/S3Flow.ts
@@ -0,0 +1,32 @@
+const flowConfig = {
+  "id": "cc895063-2407-46c7-b6c7-7987dd6bc887",
+  "name": "S3 Flow",
+  "nodes": [
+    {
+      "id": "triggerNode_1",
+      "data": {},
+      "type": "triggerNode",
+      "position": {
+        "x": 0,
+        "y": 0
+      }
+    }
+  ],
+  "edges": [],
+  "status": "active",
+  "created_at": "2026-03-15T01:29:18.707368+00:00"
+};
+
+export async function getNodesAndEdges(): Promise<{
+    nodes: Record<string, any>[],
+    edges: Record<string, any>[],
+}> {
+    return {
+        nodes: flowConfig.nodes,
+        edges: flowConfig.edges,
+    }
+}
+
+export async function getFlowConfig(): Promise<Record<string, any>> {
+    return flowConfig;
+}
