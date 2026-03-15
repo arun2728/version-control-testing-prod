ebe197d96373e5eea5e6391d14c5e037f3b11eba
accounts-lamatic <accounts@lamatic.ai>
2026-03-15 07:15:08 +0530

    [LAMATIC-COMMIT] Updated flow

--- lamatic/flows/S3Flow.ts
+++ lamatic/flows/S3Flow.ts
@@ -4,15 +4,65 @@ const flowConfig = {
   "nodes": [
     {
       "id": "triggerNode_1",
-      "data": {},
+      "data": {
+        "modes": {},
+        "nodeId": "s3Node",
+        "values": {
+          "id": "triggerNode_1",
+          "globs": [
+            "**"
+          ],
+          "bucket": "airbyte-test-qw12-2",
+          "nodeName": "S3",
+          "strategy": "auto",
+          "syncMode": "incremental",
+          "start_date": "",
+          "credentials": "S3 - Airbyte New",
+          "cronExpression": "0 0 00 ? * 1 * UTC",
+          "days_to_sync_if_history_is_full": "3"
+        },
+        "trigger": true
+      },
       "type": "triggerNode",
+      "measured": {
+        "width": 216,
+        "height": 93
+      },
       "position": {
         "x": 0,
         "y": 0
-      }
+      },
+      "selected": true
+    },
+    {
+      "id": "plus-node-addNode_triggerNode_1340",
+      "data": {
+        "label": "+",
+        "nodeId": "addNode",
+        "values": {}
+      },
+      "type": "addNode",
+      "measured": {
+        "width": 216,
+        "height": 100
+      },
+      "position": {
+        "x": 0,
+        "y": 130
+      },
+      "selected": false
     }
   ],
-  "edges": [],
+  "edges": [
+    {
+      "id": "triggerNode_1-plus-node-addNode_triggerNode_1340",
+      "type": "defaultEdge",
+      "source": "triggerNode_1",
+      "target": "plus-node-addNode_triggerNode_1340",
+      "sourceHandle": "bottom",
+      "targetHandle": "top"
+    }
+  ],
   "status": "active",
   "created_at": "2026-03-15T01:29:18.707368+00:00"
 };
