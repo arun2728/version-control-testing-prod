d52edaf7aebc77a1603ef248522fce475c17f983
accounts-lamatic <accounts@lamatic.ai>
2026-03-16 19:59:24 +0530

    [LAMATIC-COMMIT] Created flow

---
+++ lamatic/flows/MySQLData.ts
@@ -0,0 +1,32 @@
+const flowConfig = {
+  "id": "65ca4e95-1c8f-4bef-9ce2-9aa6759cc5d3",
+  "name": "MySQL Data",
+  "nodes": [
+    {
+      "id": "triggerNode_1",
+      "data": {},
+      "type": "triggerNode",
+      "position": {
+        "x": 0,
+        "y": 0
+      }
+    }
+  ],
+  "edges": [],
+  "status": "active",
+  "created_at": "2026-03-16T14:29:23.045288+00:00"
+};
+
+export async function getNodesAndEdges(): Promise<{
+    nodes: Record<string, any>[],
+    edges: Record<string, any>[],
+}> {
+    return {
+        nodes: flowConfig.nodes,
+        edges: flowConfig.edges,
+    }
+}
+
+export async function getFlowConfig(): Promise<Record<string, any>> {
+    return flowConfig;
+}
