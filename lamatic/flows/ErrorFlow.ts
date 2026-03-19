13ca6dbc7d4522538a5f26794f82b026453e0f2f
accounts-lamatic <accounts@lamatic.ai>
2026-03-19 19:06:25 +0530

    [LAMATIC-COMMIT] Updated flow

--- lamatic/flows/ErrorFlow.ts
+++ lamatic/flows/ErrorFlow.ts
@@ -8,9 +8,10 @@ const flowConfig = {
         "modes": {},
         "nodeId": "graphqlNode",
         "values": {
+          "id": "triggerNode_1",
           "nodeName": "API Request",
           "responeType": "realtime",
-          "advance_schema": "{\"sampleInput\":\"string\"}"
+          "advance_schema": "{\n  \"credentialId\": \"string\",\n  \"model_name\": \"string\",\n  \"provider\": \"string\"\n}"
         },
         "trigger": true
       },
@@ -26,22 +27,42 @@ const flowConfig = {
       "selected": false
     },
     {
-      "id": "apiNode_807",
+      "id": "LLMNode_366",
       "data": {
         "label": "New",
-        "logic": [],
         "modes": {},
-        "nodeId": "apiNode",
+        "nodeId": "LLMNode",
         "values": {
-          "id": "apiNode_807",
-          "url": "https://webhook.site/e7fe24c7-a575-440e-94b2-f000a5a6e7da",
-          "body": "{{batchNode_692.output}}",
-          "method": "POST",
-          "headers": "",
-          "retries": "0",
-          "nodeName": "API",
-          "retry_deplay": "0",
-          "convertXmlResponseToJson": false
+          "id": "LLMNode_366",
+          "tools": [],
+          "prompts": [
+            {
+              "id": "187c2f4b-c23d-4545-abef-73dc897d6b7b",
+              "role": "system",
+              "content": "Just say hi"
+            },
+            {
+              "id": "187c2f4b-c23d-4545-abef-73dc897d6b7d",
+              "role": "user",
+              "content": "Write your prompt here"
+            }
+          ],
+          "memories": "[]",
+          "messages": "[]",
+          "nodeName": "Generate Text",
+          "attachments": "",
+          "credentials": "",
+          "generativeModelName": [
+            {
+              "type": "generator/text",
+              "params": {},
+              "configName": "configA",
+              "model_name": "gpt-4o-mini",
+              "credentialId": "3a46e042-9ced-4784-b392-fc240a0c58ec",
+              "provider_name": "openai",
+              "credential_name": "OpenAI"
+            }
+          ]
         }
       },
       "type": "dynamicNode",
@@ -51,59 +72,9 @@ const flowConfig = {
       },
       "position": {
         "x": 0,
-        "y": 260
-      },
-      "selected": false
-    },
-    {
-      "id": "batchEndNode_757",
-      "data": {
-        "label": "batchEndNode node",
-        "modes": {},
-        "nodeId": "batchEndNode",
-        "values": {
-          "nodeName": "Batch End",
-          "connectedTo": "batchNode_692"
-        }
-      },
-      "type": "batchEndNode",
-      "measured": {
-        "width": 216,
-        "height": 93
-      },
-      "position": {
-        "x": 0,
-        "y": 390
-      }
-    },
-    {
-      "id": "batchNode_692",
-      "data": {
-        "label": "batchNode node",
-        "modes": {},
-        "nodeId": "batchNode",
-        "values": {
-          "id": "batchNode_692",
-          "endValue": 10,
-          "nodeName": "Batch",
-          "increment": 1,
-          "connectedTo": "batchEndNode_757",
-          "iterateOver": "range",
-          "initialValue": "1",
-          "iteratorValue": "[]",
-          "concurrencyLimit": "2"
-        }
-      },
-      "type": "batchNode",
-      "measured": {
-        "width": 216,
-        "height": 93
-      },
-      "position": {
-        "x": 0,
         "y": 130
       },
-      "selected": true
+      "selected": false
     },
     {
       "id": "responseNode_triggerNode_1",
@@ -117,7 +88,7 @@ const flowConfig = {
           "nodeName": "API Response",
           "webhookUrl": "",
           "retry_delay": "0",
-          "outputMapping": "{\n  \"data\": \"{{batchEndNode_757.output.batchOutput}}\"\n}"
+          "outputMapping": "{\n  \"response\": \"{{LLMNode_366.output.generatedResponse}}\"\n}"
         },
         "isResponseNode": true
       },
@@ -128,70 +99,25 @@ const flowConfig = {
       },
       "position": {
         "x": 0,
-        "y": 650
+        "y": 260
       },
-      "selected": false
-    },
-    {
-      "id": "plus-node-addNode_236641",
-      "data": {
-        "label": "+",
-        "nodeId": "addNode",
-        "values": {}
-      },
-      "type": "addNode",
-      "measured": {
-        "width": 216,
-        "height": 100
-      },
-      "position": {
-        "x": 0,
-        "y": 520
-      }
+      "selected": true
     }
   ],
   "edges": [
     {
-      "id": "triggerNode_1-batchNode_692-777",
+      "id": "triggerNode_1-LLMNode_366-718",
       "type": "defaultEdge",
       "source": "triggerNode_1",
-      "target": "batchNode_692",
+      "target": "LLMNode_366",
       "sourceHandle": "bottom",
       "targetHandle": "top"
     },
     {
-      "id": "batchEndNode_757-plus-node-addNode_236641-385",
+      "id": "LLMNode_366-responseNode_triggerNode_1-449",
       "type": "defaultEdge",
-      "source": "batchEndNode_757",
-      "target": "plus-node-addNode_236641",
-      "sourceHandle": "bottom",
-      "targetHandle": "top"
-    },
-    {
-      "id": "plus-node-addNode_236641-responseNode_triggerNode_1-665",
-      "type": "defaultEdge",
-      "source": "plus-node-addNode_236641",
+      "source": "LLMNode_366",
       "target": "responseNode_triggerNode_1",
-      "sourceHandle": "bottom",
-      "targetHandle": "top"
-    },
-    {
-      "id": "batchNode_692-apiNode_807-914",
-      "data": {
-        "condition": "Batch Start",
-        "invisible": true
-      },
-      "type": "conditionEdge",
-      "source": "batchNode_692",
-      "target": "apiNode_807",
-      "sourceHandle": "bottom",
-      "targetHandle": "top"
-    },
-    {
-      "id": "apiNode_807-batchEndNode_757-710",
-      "type": "defaultEdge",
-      "source": "apiNode_807",
-      "target": "batchEndNode_757",
       "sourceHandle": "bottom",
       "targetHandle": "top"
     },
@@ -202,30 +128,6 @@ const flowConfig = {
       "target": "responseNode_triggerNode_1",
       "sourceHandle": "to-response",
       "targetHandle": "from-trigger"
-    },
-    {
-      "id": "batchNode_692-batchEndNode_757-693",
-      "data": {
-        "condition": "Batch"
-      },
-      "type": "loopEdge",
-      "source": "batchNode_692",
-      "target": "batchEndNode_757",
-      "selected": false,
-      "sourceHandle": "bottom",
-      "targetHandle": "top"
-    },
-    {
-      "id": "batchEndNode_757-batchNode_692-406",
-      "data": {
-        "condition": "Batch",
-        "invisible": true
-      },
-      "type": "loopEdge",
-      "source": "batchEndNode_757",
-      "target": "batchNode_692",
-      "sourceHandle": "bottom",
-      "targetHandle": "top"
     }
   ],
   "status": "active",
