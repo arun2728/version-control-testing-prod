9d5741820ee4301fb86a8dc44d0ab0ee02a8fd5c
accounts-lamatic <accounts@lamatic.ai>
2026-03-30 13:33:55 +0530

    [LAMATIC-COMMIT] Created flow

---
+++ lamatic/flows/RefinedSunset.ts
@@ -0,0 +1,32 @@
+const flowConfig = {
+  "id": "b4ccb56c-6cd0-40b6-82fa-84285d73909f",
+  "name": "Refined Sunset",
+  "nodes": [
+    {
+      "id": "triggerNode_1",
+      "data": {},
+      "type": "triggerNode",
+      "position": {
+        "x": 0,
+        "y": 0
+      }
+    }
+  ],
+  "edges": [],
+  "status": "active",
+  "created_at": "2026-03-30T08:03:53.467923+00:00"
+};
+
+export async function getNodesAndEdges(): Promise<{
+    nodes: Record<string, any>[],
+    edges: Record<string, any>[],
+}> {
+    return {
+        nodes: flowConfig.nodes,
+        edges: flowConfig.edges,
+    }
+}
+
+export async function getFlowConfig(): Promise<Record<string, any>> {
+    return flowConfig;
+}
